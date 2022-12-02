4f1e1c0b72388868212d43d8ebde87417ee85ec1
Nathalia Bruno <nathi.bruno@gmail.com>
2022-12-02 20:44:37 -0300

    setup styled components and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div`
+  font-family: 'Work Sans';
+`;
 
 export function App(): JSX.Element {
-  return <div data-testid="greetings-container">Welcome to the Origin THA</div>;
+  return (
+    <Root data-testid="greetings-container">Welcome to the Origin THA</Root>
+  );
 }
