0102458e0dcda246a79eee40c2549ab3821ce8eb
Nathalia Bruno <nathi.bruno@gmail.com>
2022-12-03 16:58:31 -0300

    Add amount input with mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from './components/Button';
+import InputAmount from './components/InputAmount';
 
 const Root = styled.div`
   font-family: 'Work Sans';
@@ -10,7 +11,12 @@ export function App(): JSX.Element {
   return (
     <Root data-testid="greetings-container">
       Welcome to the Origin THA
-      <Button type="button" color="white" text="Confirm" bgColor="#1B31A8" />
+      <div>
+        <Button type="button" color="white" text="Confirm" bgColor="#1B31A8" />
+      </div>
+      <div>
+        <InputAmount label="Total amount" />
+      </div>
     </Root>
   );
 }
---
+++ src/components/InputAmount.tsx
@@ -0,0 +1,84 @@
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+import MaskedInput from 'react-text-mask';
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+
+import moneySvg from '../assets/icons/money.svg';
+
+interface InputProps {
+  label: string;
+}
+
+const maskOptions = {
+  prefix: '',
+  sufix: '',
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ',',
+  allowDecimal: true,
+  decumalSymbal: '.',
+  decimalLimit: 2,
+  allowNegative: false,
+  allowLeadingZeros: false,
+};
+
+const InputAmount = (props: InputProps): ReactElement => {
+  const { label } = props;
+  const currencyMask = createNumberMask({
+    ...maskOptions,
+  });
+
+  const InputAmount = styled.div`
+    position: relative;
+  `;
+  const InputLabel = styled.label`
+    font-family: 'Work Sans';
+    display: block;
+    font-size: 0.8rem;
+    line-height: 1.1rem;
+    color: #1e2a32;
+    margin-bottom: 0.4rem;
+  `;
+
+  const InputFieldWrapper = styled.label`
+    border: 1px solid #e9eef2;
+    border-radius: 4px;
+    position: relative;
+    padding: 0.9rem 0.9rem 0.9rem 0.9rem;
+    display: inline-flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    &:before {
+      content: url('${moneySvg}');
+      width: 1.9rem;
+    }
+  `;
+
+  const InputField = styled(MaskedInput)`
+    display: inline-block;
+    width: calc(100% - 3.7rem);
+    border: none;
+    outline: none;
+    width: 100%;
+    font-family: 'Rubik';
+    font-size: 1.2rem;
+    line-height: 1.9rem;
+    color: #4d6475;
+  `;
+
+  return (
+    <InputAmount className="Amount">
+      <InputLabel htmlFor="amount">{label}</InputLabel>
+      <InputFieldWrapper htmlFor="amount">
+        <InputField
+          mask={currencyMask}
+          inputMode="numeric"
+          id="amount"
+          name="amount"
+        />
+      </InputFieldWrapper>
+    </InputAmount>
+  );
+};
+
+export default InputAmount;
